ac20c619df6ebdab6cfb74f548c402021953d1c0
Shilov Aleksey <sau0kherson@gmail.com>
2022-07-24 13:13:43 +0300

    start

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+		"github.com/gin-gonic/gin"
+
+func main() {
+
+	fmt.Println("test")
+
+	//gin.
+	// router := gin.Default()
+
+	// router.GET("/someGet", getting)
+	// router.POST("/somePost", posting)
+	// router.PUT("/somePut", putting)
+	// router.DELETE("/someDelete", deleting)
+	// router.PATCH("/somePatch", patching)
+	// router.HEAD("/someHead", head)
+	// router.OPTIONS("/someOptions", options)
+
+	// // By default it serves on :8080 unless a
+	// // PORT environment variable was defined.
+	// router.Run()
+}
